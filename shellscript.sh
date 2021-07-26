353df8fefb1ab7b5f225ad8ca54b56504b7efde7
rodigb <44157518+rodigb@users.noreply.github.com>
2021-07-26 13:43:49 +0100

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo "installing the website into a new directory..."
+echo "INSTALLING WEBSITE INTO NEW DIRECTORY"
 sudo apt-get install git #installing git
 mkdir ~/git #making the directory for the website
 cd ~/git #navigating to it
 git clone https://github.com/rodigb/e-commerce_website.git #downloading the website from github
 
 
-echo "website installation complete, now installing xampp..."
+echo "WEBSITE INSTALLATION COMPLETE, NOW INSTALLING XAMPP..."
 mkdir ~/xampinstall
 cd ~/xampinstall #make directory to install xampp
 wget https://www.apachefriends.org/xampp-files/8.0.8/xampp-linux-x64-8.0.8-1-installer.run #download link
@@ -14,4 +14,4 @@ chmod 755 xampp-linux-x64-8.0.8-1-installer.run
 ls -l xampp-linux-x64-8.0.8-1-installer.run
 sudo ./xampp-linux-x64-8.0.8-1-installer.run #run the installer
 
-echo "installation successfull, now move the website repository into htdocs manually."
+echo "INSTALLATION COMPLETE. MOVE WEBSITE FOLDER INTO HTDOCS: sudo mv /home/[user]/git/opt/lampp/htdocs"
